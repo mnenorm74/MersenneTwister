f32998ac3af66bfc1d93a88bfdf3444c4208d54d
Olga Bursina <bursina.olya@yandex.ru>
2020-04-07 11:19:01 +0500

    Initial commit

---
+++ MersenneTwister/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MersenneTwister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
